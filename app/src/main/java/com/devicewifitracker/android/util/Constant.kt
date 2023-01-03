26e5ba1a68e70d3adf935d7ca9169f55e32d98f9
wanghao <w_hao1987@163.com>
2023-01-04 07:51:51 +0800

    更改配置

--- app/src/main/java/com/devicewifitracker/android/util/Constant.kt
+++ app/src/main/java/com/devicewifitracker/android/util/Constant.kt
@@ -7,10 +7,10 @@ object Constant {
     val COLLECT_LIST_KEY = "collect_list"
     val AGREEMENT_KEY = "agreement"
 //    val TERM_OF_USE_URL = "https://sites.google.com/view/bluetooth-finder-termsofuse/home"
-    val TERM_OF_USE_URL = "https://sites.google.com/view/pinholefinder-termsofuse/home"
+    val TERM_OF_USE_URL = "https://sites.google.com/view/device-detector-terms-of-use-a/homepage"
 //    val PRIVAY_POLICY = "https://sites.google.com/view/bluetooth-finder-privacypolicy/home"
-    val PRIVAY_POLICY = "https://sites.google.com/view/pinholefinder-privacypolicy/home"
-    val EMAIL ="gudashan98@163.com"
+    val PRIVAY_POLICY = "https://sites.google.com/view/device-privacypolicys/homepage"
+    val EMAIL ="liniu127318317@gmail.com"
 //    val EMAIL ="517308295@qq.com"
 
 
